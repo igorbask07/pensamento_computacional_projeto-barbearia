3ffaaea581afb9dce1fc5cfc9ab97b70a3a652d3
AllanRik07 <allanrichard2911@gmail.com>
2026-03-02 16:53:57 -0300

    update upgrade reorganize rename
    exemplo de código

---
+++ projeto_app_Allan.py
@@ -0,0 +1,63 @@
+'''
+
+CRUD
+
+Barbearia
+
+Organizar e gerenciar clientes, agebdamentos 
+e serviços por meio das operações de cadastro,
+consulta, atualização e exclusão de dados.
+
+'''
+
+print('hello word!')
+
+input('pressione Enter para continuar...' )
+
+nome_cliente = input('qual é o seu nome?:')
+
+
+print(f'Olá, {nome_cliente}! Seja Bem-vindo(a) a nossa barbearia!')
+
+print('Bem-vindo!')
+
+# input('Pressione Enter para continuar... ')
+nome_cliente = input('Qual é o seu nome:')
+print (f"bem vindo {nome_cliente}!")
+
+nome_cliente = input ('Qual a sua turma:')
+
+nome_conta = input ('Digite sua conta:')
+print(f'{nome_conta} Correto agora entre com sua senha ')
+input  ('Digite sua senha:')
+
+nome_cliente = print ('Acesso Permitido')
+
+print("\n=== BARBEARIA ===")
+print("1. Cadastrar Cliente")
+print("2. Meu Perfil")
+print("3. Agendar Serviço")
+print("4. Meus Agendamentos")
+print("5. Serviços e Preços")
+print("6. Escolher Barbeiro")
+print("7. Histórico e Pagamentos")
+print('8.notificações' )
+
+
+while True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
