8abc104fec3b86972551e2a741e8b5c0427f5321
Milena Cavalcante <milena.cavalcante0111@gmail.com>
2026-03-02 16:53:49 -0300

    update
    exemplo de código

---
+++ projeto_app_milena.py
@@ -0,0 +1,50 @@
+'''
+
+CRUD
+Barbearia
+
+ Organizar e gerenciar clientes, agendamentos
+ e serviços por meio das operações de cadastro,
+ consulta, atualização e exclusão de dados.
+
+
+ print("\n=== BARBEARIA ===")
+ print("1. Cadastrar Cliente")
+ print("2. Meu Perfil")
+ print("3. Agendar Serviço")
+ print("4. Meus Agendamentos")
+ print("5. Serviços e Preços")
+ print("6. Escolher Barbeiro")
+ print("7. Histórico e Pagamentos")
+ print("8. Avaliações")
+ print("9. Notificações")
+ print("0. Sair")
+
+while true:
+
+escolha = input("\nEscolha uma opção: ")
+
+   if escolha == '1':
+ 
+print("Cadastrar Cliente...")
+
+ 
+'''
+
+
+print('Hello world!')
+
+# input('Precione o Enter para sair...')
+
+nome_cliente = input('inserir seu nome:')
+
+
+print(f'Olá, {nome_cliente}! Seja bem-vindo(a) a nossa barbearia!')
+
+
+
+nome_conta = input ('Digite sua conta:')
+print(f'{nome_conta} Correto agora entre com sua senha ')
+input  ('Digite sua senha:')
+
+acesso_menu = print ('Acesso Permitido')
